a3fc13f67bb32377ac3df85a693939e45920dd54
Iker-Costa <iker.costa@nemergent-solutions.com>
2021-12-08 13:04:15 +0100

    Back up operation if no HTTP server is listening

--- report.py
+++ report.py
@@ -1,3 +1,4 @@
+import json
 from multiprocessing import Queue
 from typing import List
 
@@ -7,7 +8,13 @@ from config import WBTSBotConfig
 from model import Device
 
 
+def back_up(backup_n: int, devs_dict: dict) -> None:
+    with open(f'backups/{backup_n}backup.json', 'w') as fd:
+        json.dump(devs_dict, fd)
+
+
 def http_reporter(queue: Queue, conf: WBTSBotConfig) -> None:
+    back_n: int = 0
     while True:
         devs: List[Device] = queue.get()
         devs_dict = {'location': conf.LOCATION, 'devices': [dev.__dict__ for dev in devs]}
@@ -16,4 +23,5 @@ def http_reporter(queue: Queue, conf: WBTSBotConfig) -> None:
             requests.post(f'http://{conf.SERVER_IP}:{conf.SERVER_HTTP_PORT}/{conf.SERVER_POST_PATH}', json=devs_dict)
 
         except OSError:
-            print("Couldn't connect to WBTS server")
+            back_up(back_n, devs_dict)
+            back_n += 1
